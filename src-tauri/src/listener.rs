acd0a41e0ea39c76a49a5df1e41d2c0fe37d5755
Matheus Kahlis <kahlis.dev@gmail.com>
2025-08-09 13:56:41 -0300

    fix: Removed unused last_keys

--- src-tauri/src/listener.rs
+++ src-tauri/src/listener.rs
@@ -6,7 +6,6 @@ use tauri::{AppHandle, Emitter, Manager};
 pub fn start_keyboard_listener(app_handle: AppHandle) {
     thread::spawn(move || {
         let device_state = DeviceState::new();
-        let mut last_keys: HashSet<Keycode> = HashSet::new();
 
         loop {
             let keys: HashSet<Keycode> = device_state.get_keys().into_iter().collect();
@@ -14,7 +13,6 @@ pub fn start_keyboard_listener(app_handle: AppHandle) {
             if keys.contains(&Keycode::LControl)
                 && keys.contains(&Keycode::F12)
                 && keys.contains(&Keycode::LShift)
-                && (!last_keys.contains(&Keycode::LControl) || !last_keys.contains(&Keycode::F12))
             {
                 let window = app_handle.get_webview_window("main").unwrap();
                 if window.is_visible().unwrap() {
@@ -22,18 +20,6 @@ pub fn start_keyboard_listener(app_handle: AppHandle) {
                 } else {
                     let _ = window.show();
                 }
-            }
-
-            if keys != last_keys {
-                for key in keys.difference(&last_keys) {
-                    let _ = app_handle.emit("key_event", format!("Pressed: {:?}", key));
-                }
-
-                for key in last_keys.difference(&keys) {
-                    let _ = app_handle.emit("key_event", format!("Released: {:?}", key));
-                }
-
-                last_keys = keys;
             }
 
             thread::sleep(Duration::from_millis(50));
