60460ea87610979bb28fb3068dba356cc55439b8
Matheus Kahlis <kahlis.dev@gmail.com>
2025-08-10 00:30:10 -0300

    feat: Heroui implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,20 @@
-import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
-import { listen, UnlistenFn, Event } from "@tauri-apps/api/event";
-import "./App.css";
-import { BooleanRecordView } from "./BooleanView";
-import { getCurrentWindow } from "@tauri-apps/api/window";
+import { Route, Routes } from "react-router-dom";
 
-// Definição dos tipos
-interface KeyEvent {
-  key: string;
-  state: "Press" | "Release";
-  timestamp: number;
-}
-
-// Teclas que queremos monitorar
-const interestKeys: Record<string, boolean> = {
-  ControlLeft: false,
-  AltLeft: false,
-  ShiftLeft: false,
-  F12: false,
-  Space: false,
-  KeyW: false, // Tecla W
-  KeyA: false, // Tecla A
-  KeyS: false, // Tecla S
-  KeyD: false, // Tecla D
-};
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
 
 function App() {
-  const [keys, setKeys] = useState<Record<string, boolean>>(interestKeys);
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-  const [activeKeys, setActiveKeys] = useState<string[]>([]);
-  const [windowActive, setWindowActive] = useState<boolean>(true);
-
-  listen<KeyEvent>("toggle_request", async (_) => {
-    if (windowActive) {
-      await getCurrentWindow().hide();
-    } else {
-      await getCurrentWindow().show();
-    }
-    setWindowActive(!windowActive);
-  });
-
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-      <div className="keyboard-status">
-        <h2>Keyboard Status</h2>
-        <div className="active-keys">
-          windowActive: {windowActive ? "Active" : "Inactive"}
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+    </Routes>
   );
 }
 
